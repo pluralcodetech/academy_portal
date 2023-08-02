908200af238fcdd6c07411434bec624037a0396b
cycodedconcept <cycodedconcept@gmail.com>
2023-08-02 18:07:01 +0100

    added advisor page

--- script.js
+++ script.js
@@ -3853,3 +3853,26 @@ event.preventDefault();
     .catch(error => console.log('error', error));
 }
 
+
+var bar = $("span");
+var p = $("p");
+
+var width = bar.attr("style");
+width = width.replace("width:", "");
+width = width.substr(0, width.length - 1);
+
+var interval;
+var start = 0;
+var end = parseInt(width);
+var current = start;
+
+var countUp = function () {
+  current++;
+  p.html(current + "%");
+
+  if (current === end) {
+    clearInterval(interval);
+  }
+};
+
+interval = setInterval(countUp, 1000 / (end + 1));
